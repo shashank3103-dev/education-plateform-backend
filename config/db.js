5502fbe3799d3573a7d7b5a672b95eb3a36cd8fa
shashank3103-dev <shashankshakya2015@gmail.com>
2025-07-29 18:06:39 +0530

    refactor: Update database configuration to use DB_URL for Sequelize connection

--- config/db.js
+++ config/db.js
@@ -2,16 +2,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     logging: false
+//   }
+// );
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: "mysql",
+});
+
 
 
 module.exports = sequelize ;  // Make sure to export as an object
