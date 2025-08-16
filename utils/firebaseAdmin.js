6f36966d80a05207f997a596cc8ccea6e529f219
shashank3103-dev <shashankshakya2015@gmail.com>
2025-08-17 01:26:43 +0530

    done

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -8,16 +8,39 @@
 // }
 
 // module.exports = admin;
+
+// const admin = require("firebase-admin");
+
+// if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+//   throw new Error("FIREBASE_SERVICE_ACCOUNT env var is not set!");
+// }
+
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
+
 const admin = require("firebase-admin");
 
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
   throw new Error("FIREBASE_SERVICE_ACCOUNT env var is not set!");
 }
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} catch (err) {
+  console.error("❌ Failed to parse FIREBASE_SERVICE_ACCOUNT env var");
+  throw err;
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+console.log("✅ Firebase Admin initialized");
 
 module.exports = admin;
