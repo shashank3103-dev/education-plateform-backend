10f29d55ae5031b325e7216fbddacd3067f1c621
shashank3103-dev <shashankshakya2015@gmail.com>
2025-08-17 01:03:57 +0530

    firebase serivece

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json"); // <- download from Firebase console
+// const serviceAccount = require("../serviceAccountKey.json"); // <- download from Firebase console
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
